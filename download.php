e4c6b5afd237de823a32aef9f232a15a11bf3db6
iamthebj <38149226+iamthebj@users.noreply.github.com>
2019-08-08 00:44:33 +0530

    Update download.php

--- download.php
+++ download.php
@@ -11,18 +11,7 @@
 	  CURLOPT_RETURNTRANSFER => true
 	));
 	$download_invoices = curl_exec($curl);
-
-
-
-	function _Download($f_location, $f_name){
-		$file = uniqid() . '.pdf';
-	
-		file_put_contents($file,file_get_contents($f_location));
-	
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Length: ' . filesize($file));
-		header('Content-Disposition: attachment; filename=' . basename($f_name));
+tachment; filename=' . basename($f_name));
 	
 		readfile($file);
 	}
